208894ce378fb5a7c08fe7c1e05285372a8c8226
nathan.turner <reptile72@gmail.com>
2016-04-20 13:14:50 -0400

    moved data out of views to controllers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,8 @@
 /* GET about page*/
 module.exports.about = function(req, res) {
-	res.render('generic-text', { title: 'About' });
+	res.render('generic-text', { 
+		title: 'About Loc8r',
+		content: 'Loc8r was created to help people find places to sit down and get work done.\n\n Lorem ipsum dolor sit amet...'
+	
+	});
 };
